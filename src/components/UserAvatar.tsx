4a9c95dcf12cecc69b818ec7e058075a4a1441f8
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2024-10-01 11:01:27 +0700

    feat: navbar, session provider, user button

---
+++ src/components/UserAvatar.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+import avatarPlaceholder from "@/assets/avatar-placeholder.png";
+
+interface UserAvatarProps {
+  avatarUrl: string | null | undefined;
+  size?: number;
+  className?: string;
+}
+
+const UserAvatar = ({ avatarUrl, className, size }: UserAvatarProps) => {
+  return (
+    <Image
+      src={avatarUrl || avatarPlaceholder}
+      alt="User Avatar"
+      width={size ?? 48}
+      height={size ?? 48}
+      className={cn(
+        "aspect-square h-fit flex-none rounded-full bg-secondary object-cover",
+        className,
+      )}
+    />
+  );
+};
+
+export default UserAvatar;
