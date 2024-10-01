845f81ecec10e0cfbaa180b544bf7b358bcc7934
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2024-10-01 11:33:10 +0700

    feat: sidebar, bottom bar

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <main>Home page</main>;
-}
+const Home = () => {
+  return (
+    <main className="h-[200vh] w-full bg-red-50">
+      <div className="w-full">Page</div>
+    </main>
+  );
+};
+
+export default Home;
