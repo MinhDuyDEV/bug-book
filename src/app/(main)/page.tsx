4b0824692afc0652dcf6f45fb70e40b73ba4c3a3
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2024-10-12 10:37:31 +0700

    feat: trending topics sidebar

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma";
 import Post from "@/components/posts/Post";
 import { PostDataInclude } from "@/lib/types";
+import TrendsSidebar from "@/components/TrendsSidebar";
 import PostEditor from "@/components/posts/editor/PostEditor";
 
 const Home = async () => {
@@ -10,13 +11,14 @@ const Home = async () => {
   });
 
   return (
-    <main className="w-full min-w-0">
+    <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         {posts.map((post) => (
           <Post key={post.id} post={post} />
         ))}
       </div>
+      <TrendsSidebar />
     </main>
   );
 };
